c4a8a571c4767257161f55df6ef01e63d42f0cf3
cloud.huang <cloud.huang@vipshop.com>
2017-10-13 10:59:40 +0800

    项目初始化

---
+++ src/main/java/com/yunbin/gc/App.java
@@ -0,0 +1,10 @@
+package com.yunbin.gc;
+
+/**
+ * Created by cloud.huang on 17/10/13.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("app");
+    }
+}
---
+++ src/test/java/com/yunbin/gc/AppTest.java
@@ -0,0 +1,17 @@
+package com.yunbin.gc;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by cloud.huang on 17/10/13.
+ */
+public class AppTest {
+
+    @Test
+    public void test() throws Exception {
+        assertThat(2).isGreaterThan(1);
+    }
+
+}
